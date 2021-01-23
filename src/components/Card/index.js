610b9b3bb3539a9dcbf734e184024eb89a6783cf
Krystian <drozd1krystian@gmail.com>
2021-01-23 11:14:17 +0100

    styled charts

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -7,14 +7,16 @@ const Card = ({ title, balance, subValue, ...otherProps }) => {
   return (
     <div className="card">
       <div className="card_body">
-        <div className="card_col">
-          <p className="card_text">{title}</p>
-          <p className="card_text">
-            <strong>${balance}</strong>
-          </p>
-          <p className="card_text">{subValue}</p>
+        <div className="row row-align-center">
+          <div className="col-5 card_value">
+            <p className="card_text">{title}</p>
+            <p className="card_text">
+              <strong>${balance}</strong>
+            </p>
+            <p className="card_text">{subValue}</p>
+          </div>
+          <div className="col-5">{children}</div>
         </div>
-        <div className="card_col">{children}</div>
       </div>
     </div>
   );
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -40,6 +40,9 @@ const Dashboard = (props) => {
       fill: {
         colors: ["#40BC90"],
       },
+      stroke: {
+        width: [1],
+      },
       dataLabels: {
         enabled: false,
       },
@@ -64,19 +67,26 @@ const Dashboard = (props) => {
         type: "area",
       },
       fill: {
-        colors: ["#00E396"],
+        colors: ["#00E396", "#44BADC"],
+        type: ["gradient", "solid"],
       },
       stroke: {
         curve: "smooth",
+        width: [2, 0],
       },
       dataLabels: {
         enabled: false,
       },
-      colors: ["#00E396"],
+      legend: {
+        labels: {
+          colors: "#fff",
+        },
+      },
+      colors: ["#00E396", "#44BADC"],
       annotations: {
         yaxis: [
           {
-            y: 40,
+            y: 7000,
             borderColor: "#00E396",
             label: {
               borderColor: "#00E396",
@@ -84,7 +94,7 @@ const Dashboard = (props) => {
                 color: "#fff",
                 background: "#00E396",
               },
-              text: `Initial Balance: ${40}`,
+              text: `Initial Balance: ${7000}`,
             },
           },
         ],
@@ -93,18 +103,56 @@ const Dashboard = (props) => {
         show: true,
         borderColor: "#47515d",
       },
+      labels: series.monthDataSeries1.dates,
+      xaxis: {
+        type: "datetime",
+        labels: {
+          show: true,
+          rotate: 0,
+          trim: false,
+          tickPlacement: "on",
+          style: {
+            colors: "#aab8c5",
+            fontSize: "12px",
+            fontFamily: "Helvetica, Arial, sans-serif",
+            fontWeight: 400,
+            cssClass: "apexcharts-xaxis-label",
+          },
+          format: "dd/MM/yyyy",
+        },
+      },
+      yaxis: {
+        type: "datetime",
+        title: {
+          text: "$",
+          style: {
+            color: "#aab8c5",
+          },
+        },
+        labels: {
+          show: true,
+          rotate: 0,
+          style: {
+            colors: "#aab8c5",
+            fontSize: "12px",
+            fontFamily: "Helvetica, Arial, sans-serif",
+            fontWeight: 400,
+            cssClass: "apexcharts-xaxis-label",
+          },
+        },
+      },
     },
-    labels: series.monthDataSeries1.dates,
 
     series: [
       {
         name: "Balance",
         data: series.monthDataSeries1.prices,
       },
+      {
+        name: "Daily Pln",
+        data: series.monthDataSeries2.prices,
+      },
     ],
-    xaxis: {
-      type: "datetime",
-    },
   };
 
   return (
@@ -112,52 +160,28 @@ const Dashboard = (props) => {
       <div className="row">
         <div className="col-5 ">
           <Card title="Balance" balance={0} subValue={lastTradeDate}>
-            <Chart
-              options={data.options}
-              series={data.series}
-              type="bar"
-              width="100%"
-              height="100%"
-            />
+            <Chart options={data.options} series={data.series} type="bar" />
           </Card>
         </div>
         <div className="col-5 ">
           <Card title="Last Day Pnl" balance={0} subValue="0%">
-            <Chart
-              options={data.options}
-              series={data.series}
-              type="line"
-              width="100%"
-              height="100%"
-            />
+            <Chart options={data.options} series={data.series} type="line" />
           </Card>
         </div>
         <div className="col-5 ">
           <Card title="Pnl This Month" balance={0} subValue="0%">
-            <Chart
-              options={data.options}
-              series={data.series}
-              type="line"
-              width="100%"
-              height="100%"
-            />
+            <Chart options={data.options} series={data.series} type="line" />
           </Card>
         </div>
         <div className="col-5">
           <Card title="Pnl This Year" balance={0} subValue="0%">
-            <Chart
-              options={data.options}
-              series={data.series}
-              type="line"
-              width="100%"
-              height="100%"
-            />
+            <Chart options={data.options} series={data.series} type="line" />
           </Card>
         </div>
       </div>
       <div className="row ">
         <div className="balance">
-          <h4>PLN EVOLUTION FOR ACCOUNT</h4>
+          <h4 className="balance_header">PLN EVOLUTION FOR ACCOUNT</h4>
           <div className="chart">
             <Chart
               options={lineChart.options}
