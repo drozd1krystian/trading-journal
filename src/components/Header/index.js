d3ca6d8acf7c3ece0321e237e10f463b88a3dc6b
Krystian <drozd1krystian@gmail.com>
2021-02-03 10:23:50 +0100

    added link to logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./style.scss";
 import Navbar from "../Navbar";
+import { Link } from "react-router-dom";
 
 const Header = (props) => {
   return (
     <header className="header">
       <div className="container">
         <div className="header_content">
-          <h2 className="logo">TradeJournal</h2>
+          <Link to="/dashboard">
+            <h2 className="logo">TradeJournal</h2>
+          </Link>
           <Navbar />
         </div>
       </div>
