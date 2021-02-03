382dd929651441907e12e66f3a94b20139bf16a9
Krystian <drozd1krystian@gmail.com>
2021-02-03 20:52:12 +0100

    removed logger in production

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -6,7 +6,11 @@ import rootSaga from "./rootSaga";
 import thunk from "redux-thunk";
 
 const sagaMiddleware = createSagaMiddle();
-export const middlewares = [sagaMiddleware, logger, thunk];
+export const middlewares = [sagaMiddleware, thunk];
+
+if (process.env.NODE_ENV === "development") {
+  middlewares.push(logger);
+}
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
